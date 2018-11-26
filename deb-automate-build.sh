c6c29158c4d92085d56b3c7106c1a90b48792649
ctrix <mc@spamteq.com>
2018-11-26 13:43:20 +0000

    ZIP dependency missing

--- deb-automate-build.sh
+++ deb-automate-build.sh
@@ -6,7 +6,7 @@
 
 sudo apt-get update
 sudo apt-get --no-install-recommends --no-install-suggests \
-    -y install devscripts tar dpkg-dev debhelper dh-systemd libwww-perl gnupg2
+    -y install devscripts tar dpkg-dev debhelper dh-systemd libwww-perl gnupg2 unzip
 
 rm -f ../consul*.orig.tar.gz
 uscan -v --download-current-version
