fa3c209fe9e1142f21b0ca537bbcf33111cf36bb
ctrix <mc@spamteq.com>
2018-11-26 13:36:40 +0000

    New upstream

--- deb-automate-build.sh
+++ deb-automate-build.sh
@@ -4,12 +4,14 @@
 
 # This script is an utility to automate the building of the consul package.
 
-sudo apt-get -y install dpkg-dev debhelper dh-systemd devscripts libwww-perl
+sudo apt-get update
+sudo apt-get --no-install-recommends --no-install-suggests \
+    -y install devscripts tar dpkg-dev debhelper dh-systemd libwww-perl gnupg2
 
 rm -f ../consul*.orig.tar.gz
 uscan -v --download-current-version
 FILE=`ls -1 ../consul*.orig.tar.gz`
 tar zxvf ../consul*.orig.tar.gz --strip 0
 
-dpkg-buildpackage -us -uc -b
+dpkg-buildpackage -us -uc -b --ignore-builtin-builddeps
 debian/rules clean
